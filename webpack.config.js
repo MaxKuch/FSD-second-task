6839ac05ee68beca5ae97be1c2bc2f810736b606
Max <narrm4517@gmail.com>
2020-01-31 22:13:27 +0300

    Webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const isDev = process.env.NODE_env === 'development'
+const isProd = !isDev;
+console.log('idDev:', isDev)
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+const cssLoaders = extra => {
+    const loaders = [{
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            hmr: isProd,
+            reloadAll: true
+        }
+    }, 'css-loader']
+    if (extra) {
+        loaders.push(extra)
+    }
+    return loaders
+}
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './index.js'
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@styles': path.resolve(__dirname, 'src/styles')
+        }
+    },
+    optimization: optimization(),
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|eot|svg)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
